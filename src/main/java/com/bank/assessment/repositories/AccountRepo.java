7d0855f5dfa3cfa54c3b17c4af3ba66a04851136
ruben wilson <105153564+ruben-wilson@users.noreply.github.com>
2023-03-20 10:00:21 +0000

    Delete src directory

--- src/main/java/com/bank/assessment/repositories/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bank.assessment.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bank.assessment.entities.Account;
-
-public interface AccountRepo extends JpaRepository<Account, Integer> {
-  
-}
